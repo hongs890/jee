93dc41c829f45575f322fe05aba1eabf193bf4a1
hanbit <hanbit@hb2018>
2016-07-01 14:48:01 +0900

    2016-07-01

--- jee/src/sql/member.sql
+++ jee/src/sql/member.sql
@@ -7,9 +7,24 @@ create table member(
 	reg_date varchar2(20),
 	ssn varchar2(10)	
 );
+-- CREATE
 
 insert into member(id,pw,name,reg_date,ssn)
 values('lee','1','이순신','2016-07-01','800101-1');
+insert into member(id,pw,name,reg_date,ssn)
+values('hong','1','홍길동','2015-07-01','901201-1');
+insert into member(id,pw,name,reg_date,ssn)
+values('you','1','유관순','2014-07-01','010701-4');
+-- READ 
+select * from member
+where id = 'lee';
+-- UPDATE
+update member set pw = '999'
+where id = 'you';
+-- DELETE
+delete from member
+where id = 'you';
+
 
 select * from member;
 drop table member;
